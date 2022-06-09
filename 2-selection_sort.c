965a1973078111ef20c823bef4e876ab6559c261
Pierre Dureau <dureaupierre53@gmail.com>
2022-06-09 11:16:00 +0200

    optimisation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,7 +18,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		min = array[i];
-		for (j = i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < min)
 			{
